d25f04c701d9674b62c509b5167e1ed449e0f0cc
liongj <150574403+liongj@users.noreply.github.com>
2024-09-14 22:45:48 -0600

    Add files via upload

---
+++ ProyectoJunit/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class FizzBuzzTest {
+
+    private FizzBuzz game;
+
+    @Before
+    public void  before(){
+         game = new FizzBuzz();
+    }
+
+    @Test
+    public void  should_returnNumbers_whenNotAMultiple0f30r5(){
+        List<String> numbersList = game.getNumbers();
+        Assert.assertEquals( "1", numbersList.get(0));
+        Assert.assertEquals( "2", numbersList.get(1));
+        Assert.assertEquals( "7", numbersList.get(6));
+        Assert.assertEquals( "13", numbersList.get(12));
+    }
+    @Test
+    public void  should_returnFizz_whenAMultiple0f3(){
+        List<String> numbersList = game.getNumbers();
+        Assert.assertEquals( "Fizz", numbersList.get(2));
+        Assert.assertEquals( "Fizz", numbersList.get(5));
+        Assert.assertEquals( "Fizz", numbersList.get(8));
+        Assert.assertEquals( "Fizz", numbersList.get(11));
+    }
+    @Test
+    public void  should_returnFizz_whenAMultiple0f5(){
+        List<String> numbersList = game.getNumbers();
+        Assert.assertEquals( "Buzz", numbersList.get(4));
+        Assert.assertEquals( "Buzz", numbersList.get(9));
+        Assert.assertEquals( "Buzz", numbersList.get(19));
+        Assert.assertEquals( "Buzz", numbersList.get(24));
+    }
+    @Test
+    public void  should_returnFizzBuzz_whenAMultiple3and5(){
+        List<String> numbersList = game.getNumbers();
+        Assert.assertEquals( "FizzBuzz", numbersList.get(14));
+        Assert.assertEquals( "FizzBuzz", numbersList.get(29));
+        Assert.assertEquals( "FizzBuzz", numbersList.get(59));
+        Assert.assertEquals( "FizzBuzz", numbersList.get(89));
+    }
+}
---
+++ ProywctoVideo/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+
+    public static void main(String[] args){
+        double salario = 1000.00;
+        System.out.println(MoneyUtil.format(salario));
+    }
+}
